33128d89d864a73f97ddc91c26ed94feb658af60
sparg <ivangv90@gmail.com>
2024-09-20 00:24:06 +0200

    #3 Updated the version of ubuntu to 22_04 LTS.

--- main.tf
+++ main.tf
@@ -103,8 +103,8 @@ resource "azurerm_linux_virtual_machine" "tf-vm-tg" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2" # Last version in westeurope of ubuntu.
     version   = "latest"
   }
 
