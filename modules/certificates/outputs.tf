d17963b1530e78bcf4a0b08034ddb57890d04862
sparg <ivangv90@gmail.com>
2024-11-19 00:04:10 +0100

    Deleted all modules

--- modules/certificates/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_key" {
-  value     = tls_private_key.tf-vm-tg.public_key_openssh
-  sensitive = false
-}
-
-output "private_key" {
-  value     = tls_private_key.tf-vm-tg.private_key_pem
-  sensitive = true
-}
